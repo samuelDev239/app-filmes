78e7933115eb3e32437255c139ac89c9bd3c09fb
samueldev239 <samuellucianodev@gmail.com>
2023-05-05 17:20:39 -0300

    add flatlist

---
+++ src/data/filmes.js
@@ -0,0 +1,66 @@
+const DATA = 
+[
+    {
+        id:'01',
+        nome:'Adão Negro',
+        nota: '4,5',
+        imagem: 'adao.jpg'
+    },
+    {
+        id:'02',
+        nome:'Avatar',
+        nota: '5,0',
+        imagem: 'avatar.jpg'
+    },
+    {
+        id:'03',
+        nome:'Desaparecida',
+        nota: '1,5',
+        imagem: 'desaparecida.jpg'
+    },
+    {
+        id:'04',
+        nome:'Gato de Botas 3',
+        nota: '4,5',
+        imagem: 'gatos.jpg'
+    },
+    {
+        id:'05',
+        nome:'Homem Formiga',
+        nota: '3,0',
+        imagem: 'homemformiga.jpg'
+    },
+    {
+        id:'06',
+        nome:'Os incriveis',
+        nota: '5,0',
+        imagem: 'incriveis.jpg'
+    },
+    {
+        id:'07',
+        nome:'Mario',
+        nota: '5,0',
+        imagem: 'mario.jpg'
+    },
+    {
+        id:'08',
+        nome:'Megan a boneca do cão',
+        nota: '1,0',
+        imagem: 'megan.jpg'
+    },
+    {
+        id:'09',
+        nome:'Panico',
+        nota: '3,0',
+        imagem: 'panico.jpg'
+    },
+    {
+        id:'10',
+        nome:'Wandinha',
+        nota: '5,0',
+        imagem: 'wandinha.jpg'
+    },
+]
+
+export default DATA;
+
